4e5437092f76087099f9483191b8a253a5876f28
Joyal520 <roshanjoyal520@gmail.com>
2026-04-08 16:42:13 +0530

    fix: flashscreen image cropping on mobile, ignore venv, and update cover image

--- story-utils.js
+++ story-utils.js
@@ -26,7 +26,7 @@
         #story-splash-overlay img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: contain;
             animation: splashZoom ${SPLASH_DURATION + FADE_DURATION}ms ease-out forwards;
         }
 
